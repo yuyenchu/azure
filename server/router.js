10061b1e4ddcd53eac49acc26208bf359d3213fe
andrew <andrew7011616@gmail.com>
2020-07-14 16:41:59 +0800

    fix string formatting-3

--- server/router.js
+++ server/router.js
@@ -34,7 +34,7 @@ router.route('/devices')
 
 router.route('/twin')
 	.get(function (req, res) {
-		const twinUrl = 'https://hub-test1.azure-devices.net/twins/${req["id"]}?api-version=2018-06-30'
+		const twinUrl = 'https://hub-test1.azure-devices.net/twins/'+req["id"]+'?api-version=2018-06-30'
 		request.get({
 			url: twinUrl,
 			headers: hubHead
