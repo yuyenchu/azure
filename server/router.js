277eb98c8ae888cbbee75aea6a9268a6a892319c
andrew <andrew7011616@gmail.com>
2020-07-16 14:38:00 +0800

    add invoke to page

--- server/router.js
+++ server/router.js
@@ -34,7 +34,7 @@ router.route('/devices')
 	});
 
 router.route('/method/:id/:methodname/:payload')
-	.get(function (req, res) {
+	.post(function (req, res) {
 		const twinUrl = 'https://hub-test1.azure-devices.net/twins/'+req.params.id+'/methods?api-version=2020-03-13'
 		request.post({
 			url: twinUrl,
