d161adcdd0d43015c9637d8745fac51b97bf6608
andrew <andrew7011616@gmail.com>
2020-07-17 16:16:56 +0800

    layout design-42

--- server/router.js
+++ server/router.js
@@ -73,7 +73,7 @@ router.route('/queue')
 					console.log("queue "+response.statusCode);
 					if (response.statusCode<204) {
 						res.status(response.statusCode).json({
-							header: JSON.parse(response.headers),
+							header: response.headers,
 							body: JSON.parse(body)
 						});
 					} else {
