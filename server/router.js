cf2399156f536c6d586ceb9eaec82fed5a36e83a
andrew <andrew7011616@gmail.com>
2020-07-18 11:21:32 +0800

    add config & auto read data attribute

--- server/router.js
+++ server/router.js
@@ -1,29 +1,33 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
+const config = require('config');
+const hub = config.get('hub');
+const sb = config.get('serviceBus');
 
-const hubHead = {
-	'User-Agent': 'request',
-	'Content-Type': 'application/json',
-	'Authorization': 'SharedAccessSignature sr=hub-test1.azure-devices.net&sig=ttX9fT38ugddM5g%2BISLKQaHL5Y2zrq5McsDzMVJ70yA%3D&se=1626159394&skn=iothubowner'
-}
+// const hubHead = {
+// 	'User-Agent': 'request',
+// 	'Content-Type': 'application/json',
+// 	'Authorization': 'SharedAccessSignature sr=hub-test1.azure-devices.net&sig=ttX9fT38ugddM5g%2BISLKQaHL5Y2zrq5McsDzMVJ70yA%3D&se=1626159394&skn=iothubowner'
+// }
 
-const sbHead = {
-	'Content-Type': 'application/json',
-	'Authorization': 'SharedAccessSignature sr=https%3A%2F%2Ftest-serbus1.servicebus.windows.net%2Ftest-queue1&sig=aMIhuiM3C5LSSyUYtDdja%2BFc3ZydlFEr/rUXzCY3d7o%3D&se=1626327881&skn=RootManageSharedAccessKey'
-}
+// const sbHead = {
+// 	'Content-Type': 'application/json',
+// 	'Authorization': 'SharedAccessSignature sr=https%3A%2F%2Ftest-serbus1.servicebus.windows.net%2Ftest-queue1&sig=aMIhuiM3C5LSSyUYtDdja%2BFc3ZydlFEr/rUXzCY3d7o%3D&se=1626327881&skn=RootManageSharedAccessKey'
+// }
  
 router.route('/')
-    	.get(function (req, res) {
-	    	res.send("Connection success");
+	.get(function (req, res) {
+		console.log(JSON.stringify(hub));
+		res.send("Connection success");
 	});
 
 router.route('/devices')
 	.get(function (req, res) {
-		const deviceUrl = 'https://hub-test1.azure-devices.net/devices?api-version=2018-06-30'
+		const deviceUrl = 'https://'+hub.name+'.azure-devices.net/devices?api-version=2018-06-30'
 		request.get({
 			url: deviceUrl,
-			headers: hubHead
+			headers: hub.head
 		}, 	function(error,response,body){
 					console.log("devices "+response.statusCode);
 					var r={};
@@ -35,10 +39,10 @@ router.route('/devices')
 
 router.route('/method/:id/:methodname/:payload')
 	.get(function (req, res) {
-		const methodUrl = 'https://hub-test1.azure-devices.net/twins/'+req.params.id+'/methods?api-version=2020-03-13'
+		const methodUrl = 'https://'+hub.name+'.azure-devices.net/twins/'+req.params.id+'/methods?api-version=2020-03-13'
 		request.post({
 			url: methodUrl,
-			headers: hubHead,
+			headers: hub.head,
 			json: {
 					"methodName": req.params.methodname,
 					"responseTimeoutInSeconds": 200,
@@ -52,10 +56,10 @@ router.route('/method/:id/:methodname/:payload')
 
 router.route('/twin/:id')
 	.get(function (req, res) {
-		const twinUrl = 'https://hub-test1.azure-devices.net/twins/'+req.params.id+'?api-version=2018-06-30'
+		const twinUrl = 'https://'+hub.name+'.azure-devices.net/twins/'+req.params.id+'?api-version=2018-06-30'
 		request.get({
 			url: twinUrl,
-			headers: hubHead
+			headers: hub.head
 		}, 	function(error,response,body){
 					console.log("twin "+response.statusCode);
 					res.json(JSON.parse(body));
@@ -64,10 +68,10 @@ router.route('/twin/:id')
 
 router.route('/queue')
 	.get(function (req, res) {
-		const sbUrl = 'https://test-serbus1.servicebus.windows.net/test-queue1/messages/head'
+		const sbUrl = 'https://'+sb.name+'.servicebus.windows.net/'+sb.queueName+'/messages/head'
 		request.post({
 			url: sbUrl,
-			headers: sbHead
+			headers: sb.head
 		}, 	function(error, response, body) {
 					// console.log(response.headers);
 					console.log("queue "+response.statusCode);
