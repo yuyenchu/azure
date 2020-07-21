1f87d249999457ff5b5b76f9544e3bb842c2737c
andrew <andrew7011616@gmail.com>
2020-07-21 12:03:46 +0800

    add login page-2

--- server/app.js
+++ server/app.js
@@ -11,35 +11,39 @@ app.use(bodyparser.json());
 app.use(express.static(__dirname));
 
 app.use('/router', router);
-app.post('/auth', function(request, response) {
-	var username = request.body.username;
-	var password = request.body.password;
+app.post('/auth', function(req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
 	if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/home');
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
+		// connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		// 	if (results.length > 0) {
+		// 		request.session.loggedin = true;
+		// 		request.session.username = username;
+		// 		response.redirect('/home');
+		// 	} else {
+		// 		response.send('Incorrect Username and/or Password!');
+		// 	}			
+		// 	response.end();
+        // });
+        if (username=="andrew" && password=="andrew") {
+            req.session.loggedin = true;
+            req.session.username = username;
+            req.redirect('/home');
+        }
 	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
+		res.send('Please enter Username and Password!');
+		res.end();
 	}
 });
 app.get('/home', function(req, res) {
-    if (request.session.loggedin) {
+    if (req.session.loggedin) {
 		res.render('index.html');
 	} else {
-		response.send('Please login to view this page!');
+		res.send('Please login to view this page!');
 	}
-	response.end();
 });
-app.get('/', function(request, response) {
-	response.render('/login.html');
+app.get('/', function(req, res) {
+	res.render('/login.html');
 });
 
 app.listen(3000, function () {
