f49745753448ab57e48f9494d97f2f4439e5b0ee
andrew <andrew7011616@gmail.com>
2020-07-21 11:59:22 +0800

    add login page

--- server/app.js
+++ server/app.js
@@ -11,8 +11,35 @@ app.use(bodyparser.json());
 app.use(express.static(__dirname));
 
 app.use('/router', router);
-app.get('/', function(req, res) {
-    res.render('index.html');
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+app.get('/home', function(req, res) {
+    if (request.session.loggedin) {
+		res.render('index.html');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+app.get('/', function(request, response) {
+	response.render('/login.html');
 });
 
 app.listen(3000, function () {
