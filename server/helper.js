4d135a0d0b6ad7efd904b089ecc582587b3497b8
andrew <andrew7011616@gmail.com>
2020-07-31 12:28:01 +0800

    listener change to while loop-5

--- server/helper.js
+++ server/helper.js
@@ -1,3 +1,5 @@
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+module.exports = {
+  sleep: function (ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 }
