a2949c1553f9b7f53aea0692831adca2533428b9
andrew <andrew7011616@gmail.com>
2020-07-31 13:43:36 +0800

    add kill port shell-4

--- server/start.sh
+++ server/start.sh
@@ -1,5 +1,7 @@
 for ip in $(seq 3000 3003)
 do
-    kill -9 $(lsof -ti :${ip})
+    lsof -i :${ip}  &> /dev/null && result=0 || result=1
+    if [ "${result}" == 0 ]; then
+        kill -9 $(lsof -ti :${ip})
 done
 node app.js & node eventListener.js & node stateListener.js & node twinListener.js
