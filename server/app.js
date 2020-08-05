779ad83f3049edf217a4f00ccbd2275162ac25e8
andrew <andrew7011616@gmail.com>
2020-08-05 14:14:14 +0800

    add UI and function-14

--- server/app.js
+++ server/app.js
@@ -183,11 +183,13 @@ app.get('/manage', function(req, res) {
 });
 
 app.post('/addView/:id', function(req, res) {
+    console.log("addView"+req.params.id);
     if (req.session.loggedin) {
         connection.query('SELECT COUNT(*) AS result FROM viewControl WHERE username = ? AND device = ?', 
                             [req.session.loggedin, req.params.id], 
                             function(error, results, fields) {
             if (results[0]["result"] == 0) {
+                console.log("view not already exist");
                 connection.query('INSERT INTO viewControl VALUES(?,?)', 
                             [req.session.loggedin, req.params.id], 
                             function(error, results, fields) {
