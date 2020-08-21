6c0a306d799e84181a048eac556c2fa20893ec35
andrew <andrew7011616@gmail.com>
2020-08-21 13:34:10 +0800

    merge react front end-2

--- server/app.js
+++ server/app.js
@@ -186,6 +186,7 @@ app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs');
 // set static directory
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '../client/build')));
 // app.use(express.static(path.join(__dirname, '/public')));
 // setting express session for user login/logout
 app.use(session({
